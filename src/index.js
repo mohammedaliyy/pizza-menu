7aa9fe13672e0972c6b2d7eae25fe50c9d3a8bca
mohammedaliyy <future_itdeveloper@mail.ru>
2023-08-12 01:11:01 +0500

    final changes

--- src/index.js
+++ src/index.js
@@ -70,12 +70,20 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      {numPizzas && (
-        <ul className="pizzas">
-          {pizzaData.map((data) => {
-            return <Pizza pizza={data} key={data.name} />;
-          })}
-        </ul>
+      {numPizzas > 0 ? (
+        <>
+          <p>
+            Authentic Italian coisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
+          <ul className="pizzas">
+            {pizzaData.map((data) => {
+              return <Pizza pizza={data} key={data.name} />;
+            })}
+          </ul>
+        </>
+      ) : (
+        <p>We're still working on our menu. Please come back later :)</p>
       )}
     </main>
   );
@@ -91,12 +99,7 @@ function Footer() {
   return (
     <footer className="footer">
       {isOpen ? (
-        <div className="order">
-          <p>
-            We're open until {closingHour}:00. Come visit us or order online.
-          </p>
-          <button className="btn">Order</button>
-        </div>
+        <Order closingHour={closingHour} />
       ) : (
         <p>
           We're happy to welcome you between {openningHour}:00 and {closingHour}
@@ -107,21 +110,30 @@ function Footer() {
   );
 }
 
-function Pizza(props) {
+function Order({ closingHour }) {
   return (
-    <div className="pizza">
-      <img src={props.pizza.photoName} alt={props.pizza.name} />
+    <div className="order">
+      <p>We're open until {closingHour}:00. Come visit us or order online.</p>
+      <button className="btn">Order</button>
+    </div>
+  );
+}
+
+function Pizza({ pizza }) {
+  return (
+    <div className={pizza.soldOut ? "pizza sold-out" : "pizza"}>
+      <img src={pizza.photoName} alt={pizza.name} />
       <div>
-        <h3>{props.pizza.name}</h3>
-        <p>{props.pizza.ingredients}</p>
-        <span>{props.pizza.price} $</span>
-        {props.pizza.soldOut ? <p>sold out</p> : <p>exists</p>}
+        <h3>{pizza.name}</h3>
+        <p>{pizza.ingredients}</p>
+        <span>{pizza.soldOut ? "SOLD OUT" : pizza.price + "$"}</span>
       </div>
     </div>
   );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(root);
 root.render(
   <React.StrictMode>
     <App />
